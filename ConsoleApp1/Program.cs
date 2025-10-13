3ac39b7a5a2130826096f731e554db7e090e0cd9
AliLap <hisvdibru@gmail.com>
2025-10-13 07:47:46 +0300

    reupdate

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,18 +5,22 @@ namespace ConsoleApp1 {
     class Program {
 
         static void Main(){
-            Food f = new Food();
-            f.Dik.Add("Popcorn",12.5);
-            f.Dik.Add("rice", 6.50);
-            f.Dik.Add("pepsi", 5.20);
-            f.Dik.Add("pizza", 4.90);
-            f.Dik.Add("chocholate", 2.99);
+            //Food f = new Food();
+            //f.Dik.Add("Popcorn",12.5);
+            //f.Dik.Add("rice", 6.50);
+            //f.Dik.Add("pepsi", 5.20);
+            //f.Dik.Add("pizza", 4.90);
+            //f.Dik.Add("chocholate", 2.99);
 
-            foreach (KeyValuePair<string, double> d in f.Dik) {
-            
-                Console.WriteLine($"1-{ d.Key} ${d.Value}");
-            }
+            //foreach (KeyValuePair<string, double> d in f.Dik) {
 
+            //    Console.WriteLine($"1-{ d.Key} ${d.Value}");
+            //}
+
+            Console.WriteLine("WelCome To Cinema app please signin to continue");
+            Console.Write("user");
+            string NameInput = Console.ReadLine();
+            int? PassInput = int.TryParse(Console.ReadLine(), out var num) ? num : null;
 
 
 
