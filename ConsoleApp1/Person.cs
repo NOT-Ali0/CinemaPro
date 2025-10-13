382bee0fee22d5b76da35c3d4da9ac67a296e14a
Ali <tretrams@gmail.com>
2025-10-13 09:36:58 +0300

    make new file named Person -- ui

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public int PassWord { get; set; }
+
+
+        public Person(string name , int password ,string tilte) {
+
+            Name = name;
+            Title = tilte;
+            PassWord = password;
+        }
+
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,21 @@
 ﻿using System;
+using System.Security;
 
 namespace ConsoleApp1 {
 
     class Program {
 
         static void Main(){
+            //add user info
+
+            Person user1 = new Person("Ali",1976,"Cashier");
+            Person user2 = new Person("mohammed", 21412, "sales");
+            Person user3 = new Person("Raheem", 123546, "sales");
+            Person user4 = new Person("haider", 19346776, "customerservice");
+
+
+            //add food and prices info
+
             //Food f = new Food();
             //f.Dik.Add("Popcorn",12.5);
             //f.Dik.Add("rice", 6.50);
@@ -17,10 +28,34 @@ namespace ConsoleApp1 {
             //    Console.WriteLine($"1-{ d.Key} ${d.Value}");
             //}
 
-            Console.WriteLine("WelCome To Cinema app please signin to continue");
-            Console.Write("user");
-            string NameInput = Console.ReadLine();
-            int? PassInput = int.TryParse(Console.ReadLine(), out var num) ? num : null;
+            //set Input for name and pass
+            Console.WriteLine("please signin to continue                                                                     "+ DateTime.Now);
+            Console.WriteLine("--------------------------");
+
+
+
+
+            Console.Write("user name: ");
+            string UserNameInput = Console.ReadLine();
+
+            Console.Write("PassWord: ");
+            int? UserPassInput = int.TryParse(Console.ReadLine(), out var num) ? num : null;
+            //Console.WriteLine($"user name is {UserNameInput} and password is {UserPassInput}");
+
+
+            Console.WriteLine("[1] Film");
+            Console.WriteLine("[2] Food and Drinks");
+            Console.WriteLine("[3] Cancel item");
+            Console.WriteLine("[4] Pay");
+            Console.WriteLine("[5] End of the day");
+            Console.WriteLine("[6] Exit");
+
+
+
+
+
+
+
 
 
 
